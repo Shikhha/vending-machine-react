a2ce9810c8874125c9a1f3326a46ec6646108dc2
shikha <shikha.jhamb1992@gmail.com>
2021-10-31 19:51:44 -0400

    Added chosen field to products array

--- src/Data/AvailableProducts.js
+++ src/Data/AvailableProducts.js
@@ -1,8 +1,8 @@
 const AVAILABLE_PRODUCTS = [
-    {id: "Sushi", name: "Sushi", price: 1.00, amount : 15 },
-    {id: "Unagi", name: "Unagi", price: 2.00, amount : 5 },
-    {id: "Yakitori", name: "Yakitori", price: 5.00, amount : 23 },
-    {id: "Sukiyaki", name: "Sukiyaki", price: 10.00, amount : 7 }
+    {id: "Sushi", name: "Sushi", price: 1.00, amount : 15, choosen: 0 },
+    {id: "Unagi", name: "Unagi", price: 2.00, amount : 5, choosen: 0 },
+    {id: "Yakitori", name: "Yakitori", price: 5.00, amount : 23, choosen: 0 },
+    {id: "Sukiyaki", name: "Sukiyaki", price: 10.00, amount : 7, choosen: 0 }
 ];
 
 export default AVAILABLE_PRODUCTS;
